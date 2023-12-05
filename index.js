d0a11835420044b503602f034552d32b0c8e3c13
devrashed01 <devrashed01@gmail.com>
2023-12-05 10:56:29 +0600

    fix

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use("/uploads", express.static("uploads/"));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads/"));
+app.use(morgan("dev"));
 
 app.use("/api", require("./routes/api"));
 
