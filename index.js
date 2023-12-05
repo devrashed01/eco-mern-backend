d1fa437f5f47c286156283535774d32f25a73930
devrashed01 <devrashed01@gmail.com>
2023-12-05 10:45:22 +0600

    fix

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/uploads", express.static("uploads/"));
-const corsOptions = {
-  origin: ["https://eco-mern-frontend.vercel.app"],
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api", require("./routes/api"));
 
